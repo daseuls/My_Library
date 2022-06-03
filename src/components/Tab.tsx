9bc9ee7f8fd118eab8b6d3adae02b79fd8d6917a
daseuls <dls2tmfs2@naver.com>
2022-06-03 10:42:55 +0900

    feat: tab 생성

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,9 +1,24 @@
 import styled from "styled-components";
+import { IoLibraryOutline, IoSearchOutline, IoCalendarClearOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Tab = () => {
+  const navigate = useNavigate();
+
+  const navigatePage = (route: string) => {
+    navigate(route);
+  };
   return (
     <Wrapper>
-      <div>안녕</div>
+      <IconWrapper onClick={() => navigatePage("/")}>
+        <IoSearchOutline size={23} />
+      </IconWrapper>
+      <IconWrapper onClick={() => navigatePage("/library")}>
+        <IoLibraryOutline size={23} />
+      </IconWrapper>
+      <IconWrapper onClick={() => navigatePage("/calendar")}>
+        <IoCalendarClearOutline size={23} />
+      </IconWrapper>
     </Wrapper>
   );
 };
@@ -11,8 +26,16 @@ const Tab = () => {
 export default Tab;
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 3.5rem;
   background-color: pink;
   height: 13%;
   width: 100%;
   border-radius: 5rem;
+`;
+
+const IconWrapper = styled.div`
+  cursor: pointer;
 `;
