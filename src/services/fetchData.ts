731b3bc0cfe5d8ba8f8c8f0c4d1c403a1d1b20fc
daseuls <dls2tmfs2@naver.com>
2022-06-07 00:07:31 +0900

    feat: console 삭제

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
-
 const BASE_URL = `/search/book`;
 
 axios.defaults.headers.common.Authorization = "KakaoAK 06a99e77ee1303f63fa326248490a3ba";
@@ -17,8 +15,6 @@ export const getBookList = (query: string, sort: string, page: number) => {
       },
     })
     .then((res) => {
-      // eslint-disable-next-line no-console
-      console.log(res);
       return res;
     });
 };
